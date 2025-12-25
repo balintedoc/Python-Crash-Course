6ab065812dfe07d579799c735a78c374e99a5ed2
balintedoc <orosz.balint.gm@gmail.com>
2025-12-25 16:53:04 +0000

    Add files via upload

---
+++ guest_list.py
@@ -0,0 +1,63 @@
+people_invite = ["Albert Einstein", "Mark", "Elon Musk"]
+print ("These are the people who are invited to my dinner party:")
+print (people_invite)
+
+print ("Albert Einstein, I invited you to my dinner party. Hoping to see you there!")
+
+print ("Mark, I invited you to my dinner party. Hoping to see you there!")
+
+print ("Elon Musk, I invited you to my dinner party. Hoping to see you there!")
+#Second part of the exercise:
+person_that_cant_come = ("Elon Musk")
+print ("\n Sadly, Elon Musk has a lot of things to do, and he can't come.")
+people_invite.pop(2)
+
+people_invite.insert(2, "Alvin")
+
+print (people_invite)
+print ("The new person that I invited is: Alvin!")
+
+print ("\n Alvin, I invited you to my dinner party. Hoping to see you there!")
+
+print ("\n I just found a bigger table to eat at, and we can fit a few more people in it.")
+people_invite.insert(0, "Brad Pitt")
+people_invite.insert(1, "Jack")
+people_invite.append("Cristiano Ronaldo")
+print ("\n")
+
+print (people_invite)
+print ("The new people that I invited are: Brad Pitt, Jack, Cristiano Ronaldo")
+
+print ("\n Brad Pitt, I invited you to my dinner party. Hoping to see you!")
+print ("Jack, I invited you to my dinner party. Hoping to see you there!")
+print ("Cristiano Ronaldo, I invited you to my dinner party. Hoping to see you there!")
+#Third part of the exercise:
+
+print ("I just got news that the new dinner table won't arrive in time for the party, and there's only place for two people.")
+
+people_invite.pop(0)
+print (people_invite)
+print ("I am sorry to inform you Brad Pitt, that there's no place left for you at the dinner table.Hope to see you next time!")
+print ("\n")
+people_invite.pop(3)
+print (people_invite)
+print ("I am sorry to inform you Alvin, that there's no place left at the dinner table for you.Hoping to see you next time!")
+print ("\n")
+people_invite.pop(0)
+print (people_invite)
+print ("I am sorry to inform you Jack, that there's no place left at the dinner table for you.Hope to see you next time!")
+print ("\n")
+people_invite.pop(0)
+print (people_invite)
+print("I am sorry to inform you Albert Einstein, that there's no place left at the dinner table for you.Hope to see you next time!")
+
+print ("\n")
+print ("Mark, just letting you know that there's place for you so you can come!")
+
+print ("\n")
+print ("Cristiano Ronaldo, just letting you know that there's place for you so you can come!")
+
+people_invite.pop(0)
+people_invite.pop(0)
+print ("\n")
+print (people_invite)
