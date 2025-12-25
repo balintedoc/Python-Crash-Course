e77526815a5b10c017a9afef5a1e7abf97b5feec
balintedoc <orosz.balint.gm@gmail.com>
2025-12-25 19:10:59 +0000

    Add files via upload

---
+++ pizza.py
@@ -0,0 +1,7 @@
+pizzas = ("Pepperoni", "Cheese", "Ham")
+for pizza in pizzas:
+    print(f"I like {pizza.title()} pizza.")
+
+print ("I love pepperoni pizza, cheese pizza, ham pizza, stuffed crust pizza.\n")
+
+print ("I really love pizza!")
