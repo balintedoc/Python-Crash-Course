f8a0b6a907ccfa490702747ccfef51bd5f86fcd7
balintedoc <orosz.balint.gm@gmail.com>
2025-12-25 15:22:50 +0000

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,6 @@
+list = ['Mark', 'Alvin', 'Marci']
+message1 = ("Good Morning Mark.")
+message2 = ("Good Morning Alvin.")
+message3 = ("Good Morning Marci.")
+print (list)
+print (message1, message2, message3)
---
+++ own_list.py
@@ -0,0 +1,8 @@
+transportation = ["Car", "Bus", "Train", "Metro"]
+car1 = ("I would like to own a BMW M3.")
+bus1 = ("I like the bus because it has a lot of windows to look through.")
+train1 = ("I like the train because it's fast, and futuristic")
+metro1 = ("I like the metro because it's fast and red and white")
+
+print (transportation)
+print (car1, bus1, train1, metro1)
