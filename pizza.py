dc434745d60291993500066981f464f565301dbd
balintedoc <orosz.balint.gm@gmail.com>
2025-12-26 15:29:46 +0000

    Add files via upload

--- pizza.py
+++ pizza.py
@@ -1,7 +1,20 @@
-pizzas = ("Pepperoni", "Cheese", "Ham")
+pizzas = ("Pepperoni", "Cheese", "Ham", "Bacon")
 for pizza in pizzas:
     print(f"I like {pizza.title()} pizza.")
 
 print ("I love pepperoni pizza, cheese pizza, ham pizza, stuffed crust pizza.\n")
 
 print ("I really love pizza!")
+#4-11 exercise under here:
+
+friend_pizzas = ("Pepperoni", "Cheese", "Ham", "Tomato")
+
+print ("\n")
+print ("My favourite pizzas are:")
+for pizza in pizzas:
+    print ({pizza.title()})
+
+print ("\n")
+print ("My friend's favourite pizzas are:")
+for friend_pizza in friend_pizzas:
+    print ({friend_pizza.title()})
