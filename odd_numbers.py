4930510f85dd12068286dfd371e1bd95818a272a
balintedoc <orosz.balint.gm@gmail.com>
2025-12-26 09:05:34 +0000

    Add files via upload

---
+++ odd_numbers.py
@@ -0,0 +1,63 @@
+odd_numbers = list(range(3, 21, 3))
+
+
+odd_numbers_5 = list(range(5, 21, 5))
+
+
+odd_numbers_7 = list(range(7, 21, 7))
+
+
+odd_numbers_9 = list(range(9, 21, 9))
+
+odd_numbers_11 = list(range(11, 21, 11))
+
+odd_numbers_13 = list(range(13, 21, 13))
+
+odd_numbers_15 = list(range(15, 21, 15))
+
+odd_numbers_17 = list(range(17, 21, 17))
+
+odd_numbers_19 = list(range(19, 21, 19))
+
+for odd_numbers in range(3, 21, 3):
+    print (odd_numbers)
+
+print ("\n")
+
+for odd_numbers_5 in range(5, 21, 5):
+    print (odd_numbers_5)
+
+print ("\n")
+
+for odd_numbers_7 in range (7, 21, 7):
+    print (odd_numbers_7)
+
+print ("\n")
+
+for odd_numbers_9 in range (9, 21, 9):
+    print (odd_numbers_9)
+
+print ("\n")
+
+for odd_numbers_11 in range (11, 21, 11):
+    print (odd_numbers_11)
+
+print ("\n")
+
+for odd_numbers_13 in range(13, 21, 13):
+    print (odd_numbers_13)
+
+print ("\n")
+
+for odd_numbers_15 in range (15, 21, 15):
+    print (odd_numbers_15)
+
+print ("\n")
+
+for odd_numbers_17 in range (17, 21, 17):
+    print (odd_numbers_17)
+
+print ("\n")
+
+for odd_numbers_19 in range (19, 21, 19):
+    print (odd_numbers_19)
