4f329d9862ff6a3a9ca788115abcba645a4c224a
balintedoc <orosz.balint.gm@gmail.com>
2025-12-26 15:28:52 +0000

    Add files via upload

---
+++ buffet.py
@@ -0,0 +1,10 @@
+buffet_foods = ('pizza', 'pasta', 'fish', 'tiramisu', 'pancakes')
+
+for buffet_food in buffet_foods:
+    print (buffet_food.title())
+
+print ("\n")
+
+buffet_foods =('pizza', 'ice cream', 'fish', 'soup', 'pancakes')
+for buffet_food in buffet_foods:
+    print (buffet_food.title())
