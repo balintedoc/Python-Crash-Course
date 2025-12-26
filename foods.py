993c7a8976c275c63c8b42aec5006a273ff29ad6
balintedoc <orosz.balint.gm@gmail.com>
2025-12-26 15:29:06 +0000

    Add files via upload

---
+++ foods.py
@@ -0,0 +1,12 @@
+my_foods = ['pizza', 'falafel', 'carrot cake', 'red velvet cake']
+friend_foods = my_foods[0:3]
+
+
+print ("My favourite foods are:")
+for my_food in my_foods:
+    print ({my_food.title()})
+
+print ("\n")
+print ("My friend's favourite foods are:")
+for friend_food in friend_foods:
+    print ({friend_food.title()})
