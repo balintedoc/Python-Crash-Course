6e6368c6ee61c09a20f559f29c7f6b70b364c937
balintedoc <orosz.balint.gm@gmail.com>
2026-01-01 20:18:59 +0000

    Add files via upload

---
+++ alien_colors.py
@@ -0,0 +1,10 @@
+alien_colors =['yellow', 'green', 'red']
+
+if 'green' in alien_colors:
+    print("You just earned 5 Points!")
+
+if 'blue' in alien_colors:
+    print ("No output.")
+
+if alien_colors is not 'green':
+    print ("You just earned 10 Points!")
