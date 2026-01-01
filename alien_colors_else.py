82ace041bb880bc622f0246954698ddcc3cfbc2e
balintedoc <orosz.balint.gm@gmail.com>
2026-01-01 20:19:24 +0000

    Add files via upload

---
+++ alien_colors_else.py
@@ -0,0 +1,9 @@
+alien_colors =['yellow', 'pink', 'red']
+
+if 'green' in alien_colors:
+    print("You just earned 5 Points for killing a Zombie!")
+else:
+    #if 'blue' in alien_colors:
+       # print ("No output.")
+   # if alien_colors not in 'green':
+        print ("You just earned 10 Points!")
