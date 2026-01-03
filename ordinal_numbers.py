2260d37f9c8809f20f66528fc96dcce1b616647e
balintedoc <orosz.balint.gm@gmail.com>
2026-01-03 10:46:11 +0000

    Add files via upload

---
+++ ordinal_numbers.py
@@ -0,0 +1,12 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for number in numbers:
+    if number == 1:
+        print(f"1st")
+    elif number == 2:
+        print(f"2nd")
+    elif number == 3:
+        print(f"3rd")
+    else:
+        print(f"{number}th")
+        
