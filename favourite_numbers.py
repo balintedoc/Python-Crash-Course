f776e5fc5102e9fbe226fe657c35b4b791001065
balintedoc <orosz.balint.gm@gmail.com>
2026-01-04 13:44:32 +0000

    Add files via upload

---
+++ favourite_numbers.py
@@ -0,0 +1,24 @@
+favourite_numbers = {'Mark': 69, 'Tom' : 10, 'Servan' : 45, 'Boti' : 76, 'Martin' : 7}
+
+print (f"Mark's favourite number is:")
+print (favourite_numbers['Mark'])
+
+print ("\n")
+
+print (f"Tom's favourite number is:")
+print (favourite_numbers['Tom'])
+
+print ("\n")
+
+print (f"Servan's favourite number is:")
+print (favourite_numbers['Servan'])
+
+print ("\n")
+
+print (f"Boti's favourite number is:")
+print (favourite_numbers['Boti'])
+
+print ("\n")
+
+print (f"Martin's favourite number is:")
+print (favourite_numbers['Martin'])
