7b32def8667d7c54237ad8a76a114b23fcf70281
balintedoc <orosz.balint.gm@gmail.com>
2026-01-04 13:44:55 +0000

    Add files via upload

---
+++ person_dictionary.py
@@ -0,0 +1,9 @@
+Mark_information = {'first_name' : 'Paljok', 'last_name' : 'Mark', 'age' : 12, 'city' : 'Budapest'}
+
+print (Mark_information[f'first_name'])
+
+print (Mark_information[f'last_name'])
+
+print (Mark_information[f'age'])
+
+print (Mark_information[f'city'])
